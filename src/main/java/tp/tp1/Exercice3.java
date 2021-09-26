70f3f60b567157cbb664ff98b55f675620854fdf
idilsaglam <idilsaglam1@hotmail.com>
2021-09-26 19:16:09 +0200

    source code updated

--- src/main/java/tp/tp1/Exercice3.java
+++ src/main/java/tp/tp1/Exercice3.java
@@ -6,11 +6,11 @@ public class Exercice3 {
         for(int i=0; i<s.length(); i++){
             for(int j=0; j<voyelles.length; j++){
                 if(s.charAt(i)==voyelles[j] && voyelles[j]=='y'){
-                    s.charAt(i)='a';
+                    s = s.substring(0,i)+'a'+s.substring(i+1);
                     break;
                 }
                 if(s.charAt(i)==voyelles[j]){
-                    s.charAt(i)=voyelles[j+1];
+                    s = s.substring(0,i)+voyelles[j+1]+s.substring(i+1);
                 }
 
             }
@@ -19,7 +19,8 @@ public class Exercice3 {
     }
     public static void main(String[] args){
         for(int i=0; i<args.length; i++){
-            changer(args[i]);
+            System.out.println(changer(args[i]));
         }
+
     }
 }
