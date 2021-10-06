3da365347b2e6a9d9b6dfab670a6b8cc2e0fd9f0
idilsaglam <idilsaglam1@hotmail.com>
2021-10-06 11:50:23 +0200

    Plateau class updated

--- src/main/java/tp/tp2/Plateau.java
+++ src/main/java/tp/tp2/Plateau.java
@@ -1,5 +1,7 @@
 package tp.tp2;
 
+import java.util.Random;
+
 public class Plateau {
     private int hauteur;
     private int largeur;
@@ -10,6 +12,28 @@ public class Plateau {
     private int[][] adja; // le nb de mines adjacentes à chaque chase.
 
     public Plateau(int ha, int la, int mi ){
+        this.hauteur = ha;
+        this.largeur = la;
+        this.nbMines = mi;
+
+        mines = new boolean[ha+2][la+2];
+        etats = new int[ha+2][la+2];
+        adja = new int[ha+2][la+2];
+    }
+
+    private void ajouteMinsAle(){
+        Random rd = new Random();
+        int nb =0;
+        for(int c=0; c<nbMines; c++){
+          for (int i = 0; i < mines.length; i++) {
+            for (int j = 0; j < mines[i].length; j++) {
+                mines[i][j] = rd.nextBoolean();
+            }
+          }
+        }
+    }
+
+    private void calculeAdjacence(){
 
     }
 }
