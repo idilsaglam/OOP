75805ee17f0c969767fb2e71b96b7da4e210a3ec
idilsaglam <idilsaglam1@hotmail.com>
2021-10-06 15:38:21 +0200

    Case class updated

--- src/main/java/tp/tp3/Case.java
+++ src/main/java/tp/tp3/Case.java
@@ -4,6 +4,12 @@ public class Case {
     private boolean couleur;
     Pieces piece; // null si la case est vide
 
+    public Case(boolean couleur){
+        this.couleur = couleur;
+    }
+    public Case(Pieces piece){
+        this.piece = piece;
+    }
     public Pieces getPiece(){
         return this.piece;
     }
@@ -15,5 +21,20 @@ public class Case {
     }
     public void enleverPiece(){
         this.piece = null;
+    }
+
+    @Override
+    public String toString(){
+        // si la case n'est pas vide
+        if(!this.estVide()){
+            // Nous retournons la lettre initiale de la pièce.
+            return "" +this.piece.getNom().charAt(0);
+        }
+        // si la case est vide et la couleur est blanche
+        if(this.couleur){
+            return "-";
+        }
+        // si la case est vide et la couleur est noir
+        return "*";
     }
 }
