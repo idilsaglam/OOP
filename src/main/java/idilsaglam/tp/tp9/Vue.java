3896a69cd85898ffce2d69b0297d0de77d2feb32
idilsaglam <idilsaglam1@hotmail.com>
2021-11-27 21:26:55 +0100

    vue class updated

--- src/main/java/idilsaglam/tp/tp9/Vue.java
+++ src/main/java/idilsaglam/tp/tp9/Vue.java
@@ -13,16 +13,36 @@ public class Vue extends JFrame {
     setDefaultCloseOperation(EXIT_ON_CLOSE);
 
     JComponent j = new JComponent() {};
-    Container c = getContentPane();
-    j.add(c);
+    this.getContentPane().add(j);
 
     JPanel panneauColore = new JPanel();
     panneauColore.setBackground(Color.GREEN);
-    this.setContentPane(panneauColore);
+    this.getContentPane().add(panneauColore);
 
     JLabel etiqCouleur = new JLabel("Vert");
-    panneauColore.add(etiqCouleur);
-    //this.getContentPane().add(etiqCouleur, BorderLayout.CENTER);
+    etiqCouleur.setHorizontalAlignment(0);
+    panneauColore.setLayout(new BorderLayout());
+    panneauColore.add(etiqCouleur, BorderLayout.CENTER);
 
+    JPanel panneauChoix = new JPanel();
+    panneauChoix.setBackground();
+    panneauChoix.setLayout(new BorderLayout());
+    this.getContentPane().add(panneauChoix);
+    this.getContentPane().setLayout(new GridLayout());
+
+    JComponent buttonContainer = new JComponent() {};
+    panneauChoix.add(buttonContainer, BorderLayout.PAGE_END);
+
+    JButton btnMemoriser = new JButton();
+    btnMemoriser.setText("Mémoriser");
+    buttonContainer.add(btnMemoriser);
+
+    JButton btnSenRappeler = new JButton();
+    btnSenRappeler.setText("S'en rappeler");
+    buttonContainer.add(btnSenRappeler);
+
+    JButton btnComplementaire = new JButton();
+    btnComplementaire.setText("Complémentaire");
+    buttonContainer.add(btnComplementaire);
   }
 }
