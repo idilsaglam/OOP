2524231af91634a97936992aade383bffb98f7c0
idilsaglam <idilsaglam1@hotmail.com>
2021-11-29 02:39:03 +0100

    tp9 updated

--- src/main/java/idilsaglam/tp/tp9/Controleur.java
+++ src/main/java/idilsaglam/tp/tp9/Controleur.java
@@ -1,19 +1,38 @@
 /* (C)2021 */
 package idilsaglam.tp.tp9;
 
-import org.w3c.dom.events.Event;
-
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class Controleur {
   private Modele modele;
   private JSlider slider;
-  private Consumer<>
-  public void sliderMoved() {
-    int value = slider.getValue();
-    this.slider.addChangeListener((ChangeEvent e) ->{
-      e.
-    });
+  private SliderType sliderType;
+
+  public Controleur(Modele modele, JSlider slider, SliderType sliderType) {
+    this.slider = slider;
+    this.sliderType = sliderType;
+    this.modele = modele;
+    this.slider.addChangeListener(this.sliderMoved());
+  }
+
+  public ChangeListener sliderMoved() {
+    return (ChangeEvent e) -> {
+      int value = slider.getValue();
+      switch (this.sliderType) {
+        case RED -> this.modele.setColorR(value);
+        case BLUE -> this.modele.setColorB(value);
+        case ALPHA -> this.modele.setColorA(value);
+        case GREEN -> this.modele.setColorG(value);
+      }
+    };
+  }
+
+  public static enum SliderType {
+    RED,
+    GREEN,
+    BLUE,
+    ALPHA
   }
 }
--- src/main/java/idilsaglam/tp/tp9/Vue.java
+++ src/main/java/idilsaglam/tp/tp9/Vue.java
@@ -2,22 +2,22 @@
 package idilsaglam.tp.tp9;
 
 import java.awt.*;
+import java.util.Hashtable;
 import javax.swing.*;
 
 public class Vue extends JFrame {
-
-  public Controleur c = new Controleur();
   private JPanel panneauColore;
   private JPanel panneauChoix;
   private Modele m;
-  private JSlider slider;
   private JLabel etiqCouleur;
+
+  private JSlider slider1, slider2, slider3, slider4;
 
   public Vue() {
     this.setTitle("Palette");
     this.setSize(800, 600);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    this.m = new Modele(Color.red, this);
+    this.m = new Modele(Color.PINK, this);
 
     this.etiqCouleur = new JLabel(this.m.colorToRGB());
     this.panneauColore = this.initPaneauColore();
@@ -39,7 +39,6 @@ public class Vue extends JFrame {
 
   private JPanel initPaneauColore() {
     JPanel panel = new JPanel();
-    panel.setBackground(Color.GREEN);
     panel.setLayout(new BorderLayout());
     etiqCouleur.setHorizontalAlignment(0);
     panel.add(etiqCouleur, BorderLayout.CENTER);
@@ -71,15 +70,64 @@ public class Vue extends JFrame {
 
   private JPanel initSliders() {
     JPanel p = new JPanel(new GridLayout(4, 1));
-    JSlider slider1 = new JSlider();
-    JSlider slider2 = new JSlider();
-    JSlider slider3 = new JSlider();
-    JSlider slider4 = new JSlider();
+    Hashtable<Integer, JLabel> labels = new Hashtable<>();
+    for (int i = 0; i < 255; i = i + 25) {
+      labels.put(i, new JLabel("" + i));
+    }
 
-    p.add(slider1);
-    p.add(slider2);
-    p.add(slider3);
-    p.add(slider4);
+    this.slider1 = new JSlider();
+    this.slider1.setValue(this.m.getColor().getRed());
+    this.slider1.setToolTipText("Change red channel value");
+    this.slider1.setPaintTicks(true);
+    this.slider1.setMinimum(0);
+    this.slider1.setMaximum(255);
+    this.slider1.setMinorTickSpacing(5);
+    this.slider1.setMajorTickSpacing(25);
+    this.slider1.setLabelTable(labels);
+    this.slider1.setPaintLabels(true);
+
+    this.slider2 = new JSlider();
+    this.slider2.setValue(this.m.getColor().getGreen());
+    this.slider2.setToolTipText("Change green channel value");
+    this.slider2.setPaintTicks(true);
+    this.slider2.setMinimum(0);
+    this.slider2.setMaximum(255);
+    this.slider2.setMinorTickSpacing(5);
+    this.slider2.setMajorTickSpacing(25);
+    this.slider2.setLabelTable(labels);
+    this.slider2.setPaintLabels(true);
+
+    this.slider3 = new JSlider();
+    this.slider3.setValue(this.m.getColor().getBlue());
+    this.slider3.setToolTipText("Change blue channel value");
+    this.slider3.setPaintTicks(true);
+    this.slider3.setMinimum(0);
+    this.slider3.setMaximum(255);
+    this.slider3.setMinorTickSpacing(5);
+    this.slider3.setMajorTickSpacing(25);
+    this.slider3.setLabelTable(labels);
+    this.slider3.setPaintLabels(true);
+
+    this.slider4 = new JSlider();
+    this.slider4.setValue(this.m.getColor().getAlpha());
+    this.slider4.setToolTipText("Change alpha channel value");
+    this.slider4.setPaintTicks(true);
+    this.slider4.setMinimum(0);
+    this.slider4.setMaximum(255);
+    this.slider4.setMinorTickSpacing(5);
+    this.slider4.setMajorTickSpacing(25);
+    this.slider4.setLabelTable(labels);
+    this.slider4.setPaintLabels(true);
+
+    Controleur controleurRed = new Controleur(this.m, this.slider1, Controleur.SliderType.RED);
+    Controleur controleurBlue = new Controleur(this.m, this.slider2, Controleur.SliderType.GREEN);
+    Controleur controleurGreen = new Controleur(this.m, this.slider3, Controleur.SliderType.BLUE);
+    Controleur controleurAlpha = new Controleur(this.m, this.slider4, Controleur.SliderType.ALPHA);
+
+    p.add(this.slider1);
+    p.add(this.slider2);
+    p.add(this.slider3);
+    p.add(this.slider4);
     return p;
   }
 }
